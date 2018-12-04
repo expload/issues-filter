95a7f682f5a877e31eced3917c9d6d1d5082ef46
vovapolu <vovapolu@gmail.com>
2018-12-04 19:51:17 +0300

    Add github token to CLI

--- report_from_git.py
+++ report_from_git.py
@@ -19,15 +19,20 @@ parser.add_argument("--until", required=False, help="Finishing date. " + \
 parser.add_argument("--users", nargs='+', required=False, help="Github logins of users. " + \
                                                                "Only commits of these users will be printed.")
 parser.add_argument("--reverse", action="store_true", help="Reverse the order of commits.")
+parser.add_argument("--token", required=False, help="Oauth github token.")
 
 args = parser.parse_args()
 
 def download_commits(repo, user):
-    page = 0
+    page = 1
     commits = []
     while True:
         params = {"until": args.until, "since": args.since, "per_page": 100, "page": page, "author": user}
-        raw_commits = json.loads(requests.get("https://api.github.com/repos/{}/commits".format(repo), params=params).text)    
+        if args.token is not None:
+            params["access_token"] = args.token
+        raw_commits = json.loads(requests.get("https://api.github.com/repos/{}/commits".format(repo), params=params).text) 
+        if not isinstance(raw_commits, list):
+            break
         if len(raw_commits) == 0:
             break
         for rw in raw_commits:
@@ -47,8 +52,8 @@ commit_number = re.compile('#(\\d+)')
 for user in args.users:
     print()
     print(user)
-    print()
-    for repo in args.repos:
+    for repo in args.repos:        
+        print()
         print(repo)
         issues = defaultdict(list)
         for commit in download_commits(repo, user):    
