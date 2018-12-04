2ca68adf485f8fbf2d081480bcd972dbb204a8a9
vovapolu <vovapolu@gmail.com>
2018-12-04 19:27:41 +0300

    Update API: Add support for multiple repos and users, make calls to github API

--- report_from_git.py
+++ report_from_git.py
@@ -5,58 +5,69 @@ import re
 from collections import defaultdict
 import sys
 import argparse
+import requests
+import json
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--repo", "-r", required=True, help="target repository")
-parser.add_argument("--from", "-f", required=False, help="starting date")
-parser.add_argument("--to", "-t", required=False, help="finishing date")
-parser.add_argument("--user", "-u", required=False, help="target user")
-parser.add_argument("--reverse", action="store_true", help="reverse the order of commits in git log")
+parser.add_argument("--repos", nargs='+', required=True, help="Organization and repository name. " + \
+                                                               "Only commits from these repositories will be printed. Example: expload/pravda")
+parser.add_argument("--since", required=False, help="Starting date. " + \
+                                                    "Only commits after this date will be printed. Example: 2018-10-20")
+parser.add_argument("--until", required=False, help="Finishing date. " + \
+                                                    "Only commits before this date will be printed. Example: 2018-10-20")
+parser.add_argument("--users", nargs='+', required=False, help="Github logins of users. " + \
+                                                               "Only commits of these users will be printed.")
+parser.add_argument("--reverse", action="store_true", help="Reverse the order of commits.")
 
 args = parser.parse_args()
 
+def download_commits(repo, user):
+    page = 0
+    commits = []
+    while True:
+        params = {"until": args.until, "since": args.since, "per_page": 100, "page": page, "author": user}
+        raw_commits = json.loads(requests.get("https://api.github.com/repos/{}/commits".format(repo), params=params).text)    
+        if len(raw_commits) == 0:
+            break
+        for rw in raw_commits:
+            sha = rw["sha"]
+            msg = rw["commit"]["message"]
+            commits.append((sha, msg))
+        
+        page += 1
+
+    if args.reverse:
+        commits = commits[::-1]
+
+    return commits
+
 commit_number = re.compile('#(\\d+)')
 
-issues = []
-
-for line in subprocess.check_output(['git', 'log', '--pretty=format:%H~%aN~%s~%cd', '--date=format:%Y-%m-%d']).decode("utf-8").split("\n"):
-    if not "Merge pull request" in line:
-        (cid, user, message, date) = line.split("~")
-        issue = commit_number.search(message)
-        if issue is not None:
-            issues.append((user, cid, date, issue.group(0)))
-        else:
-            issues.append((user, cid, date, "none"))
-
-if args.reverse:
-    issues = issues[::-1]
-
-filtered_issues = defaultdict(list)
-
-for (user, cid, date, issue) in issues:
-    ok = True
-    if (args.user is not None) and (args.user != user):
-        ok = False
-
-    if (getattr(args, "from") is not None) and (getattr(args, "from") > date):
-        ok = False
-
-    if (args.to is not None) and (args.to < date):
-        ok = False
-
-    if ok:
-      filtered_issues[issue].append(cid)
-
-for (issue, cids) in filtered_issues.items():
+for user in args.users:
     print()
-    if issue != "none":
-        print(args.repo + "/issues/" + issue[1:])
-    else:
-        print("none issue")
+    print(user)
     print()
-    for cid in cids:
-        print(args.repo + "/commit/" + cid)
+    for repo in args.repos:
+        print(repo)
+        issues = defaultdict(list)
+        for commit in download_commits(repo, user):    
+            issue = commit_number.search(commit[1])
+            if issue is not None:
+                issue = issue.group(0)
+            else:
+                issue = "none"
+            issues[issue].append(commit[0])
+
+        for (issue, shas) in issues.items():
+            print()
+            if issue != "none":
+                print("https://github.com/{}/issues/{}".format(repo, issue[1:]))
+            else:
+                print("none issue")
+            print()
+            for sha in shas:
+                print("https://github.com/{}/commit/{}".format(repo, sha))
 
 
     
